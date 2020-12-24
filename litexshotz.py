b5859fbf9eb41bddb322d527d9cc132b78ea4f3f
discordlite1 <76547508+discordlite1@users.noreply.github.com>
2020-12-24 12:48:34 +0700

    Update litexshotz.py

--- litexshotz.py
+++ litexshotz.py
@@ -1,40 +1,88 @@
 import discord
 from discord.ext import commands
-import asyncio
-import random
-import os
-import praw
-import json
+from asyncio import sleep
+client = commands.Bot(command_prefix='-', help_command=None)
 
 
-client = commands.Bot(command_prefix = '.') 
-
+async def status():
+    while True:
+        await client.wait_until_ready()
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='𝙇𝙄𝙏𝙀-𝙢𝙚𝙢𝙗𝙚𝙧𝙨'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='LITE-members'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='𝙇𝙄𝙏𝙀-𝙢𝙚𝙢𝙗𝙚𝙧𝙨'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='LITE-members'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='𝙇𝙄𝙏𝙀-𝙢𝙚𝙢𝙗𝙚𝙧𝙨'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='LITE-members'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='𝙇𝙄𝙏𝙀-𝙢𝙚𝙢𝙗𝙚𝙧𝙨'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='LITE-members'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='𝙇𝙄𝙏𝙀-𝙢𝙚𝙢𝙗𝙚𝙧𝙨'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='LITE-members'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='𝙇𝙄𝙏𝙀-𝙢𝙚𝙢𝙗𝙚𝙧𝙨'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='LITE-members'))
+        await sleep(1)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='𝙇𝙄𝙏𝙀-𝙢𝙚𝙢𝙗𝙚𝙧𝙨'))
+        await sleep(1)
 @client.event
 async def on_ready():
-     #Setting `playing ` status
-     await client.change_presence(activity=discord.Game(name=f"on {len(client.guilds)} servers | Developer HenG"))
-     #Setting `Streaming ` status
-     await client.change_presence(activity=discord.Streaming(name="xvideo.com", url=https://www.xvideos.com/))
-     #Setting `Listening ` status
-     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.Listening, name="LITE manager"))
-     #Setting `Watching ` status
-     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Hengz developer><"))
-
-     print("Ready")
+    print(f'{client.user} has Awoken!')
+    await client.loop.create_task(status())
 
 
-     async def ch_pr():
-     	   await client.wait_until_ready()
+import discord
+from discord.ext import commands
+import datetime
 
-     	   statuses = ["a game", f"on {len(client.guilds)} servers | e!help", "discord.py"]
+from urllib import parse, request
+import re
 
-     	   while not client.is_closed():
+bot = commands.Bot(command_prefix='>', description="This is a Helper Bot")
 
-     	   	status = random.choice(statuses)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-     	   	await client.change_presence(activity=discord.Game(name=status))
+@bot.command()
+async def sum(ctx, numOne: int, numTwo: int):
+    await ctx.send(numOne + numTwo)
 
-     	   	await asyncio.sleep(10)
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem Ipsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    # embed.set_thumbnail(url=f"{ctx.guild.icon}")
+    embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
 
-    client.loop.create_task(ch_pr())
-    client.run()
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def youtube(ctx, *, search):
+    query_string = parse.urlencode({'search_query': search})
+    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
+    # print(html_content.read().decode())
+    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html_content.read().decode())
+    print(search_results)
+    # I will put just the first result, you can loop the response to show more results
+    await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
+
+@bot.listen()
+async def on_message(message):
+    if "tutorial" in message.content.lower():
+        # in this case don't respond with the word "Tutorial" or you will call the on_message event recursively
+        await message.channel.send('This is that you want http://youtube.com/fazttech')
+        await bot.process_commands(message)
+client.run('Nzg5MDkzMDUyMzYyMTI5NDY5.X9tCLA.VTbRPM6qQO7udbcz-VEwKvtXDrM')
+bot.run('Nzg5MDkzMDUyMzYyMTI5NDY5.X9tCLA.VTbRPM6qQO7udbcz-VEwKvtXDrM')
